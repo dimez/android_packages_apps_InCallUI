3acb218b878cd34998f921c3e83734eebf2d16bc
Garik Badalyan <garikb@codeaurora.org>
2013-10-17 12:37:12 -0700

    IMS-VT: Build and install libvt_jni and libimscamera_jni
    libvt_jni and libimscamera_jni are used by IMS phone.
    Build and install libvt_jni and libimscamera_jni with Phone.
    
    Change-Id: I0a962dc3891fb56dd5ee63b26cf0239bc60d33b6
    CRs-Fixed: 569101

--- Android.mk
+++ Android.mk
@@ -6,6 +6,9 @@ LOCAL_STATIC_JAVA_LIBRARIES := com.android.services.telephony.common \
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
+LOCAL_JNI_SHARED_LIBRARIES := libvt_jni libimscamera_jni
+LOCAL_REQUIRED_MODULES := libvt_jni libimscamera_jni
+
 LOCAL_PACKAGE_NAME := InCallUI
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVELEGED_MODULE := false
