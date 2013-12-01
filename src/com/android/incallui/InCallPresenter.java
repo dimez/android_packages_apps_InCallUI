084fda5457d0748fa2ab8678bb1825509d1db860
maxwen <max.weninger@gmail.com>
2013-12-01 16:46:49 +0100

    [3/3] InCallUI: allow disabling non intrusive incall ui
    Change-Id: I86bd4b46f031004fa90f1d7936263f9dabcd3860
    
    Conflicts:
    
    	src/com/android/incallui/InCallPresenter.java

--- src/com/android/incallui/InCallPresenter.java
+++ src/com/android/incallui/InCallPresenter.java
@@ -27,8 +27,13 @@ import com.google.android.collect.Sets;
 import com.google.common.base.Preconditions;
 
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
+<<<<<<< HEAD
 import android.content.ActivityNotFoundException;
+=======
+import android.provider.Settings;
+>>>>>>> e6b4d7b... [3/3] InCallUI: allow disabling non intrusive incall ui
 import android.os.PowerManager;
 
 import com.android.services.telephony.common.Call;
@@ -781,9 +786,12 @@ public class InCallPresenter implements CallList.Listener {
             mInCallActivity = null;
         }
 
+        boolean nonIntrusiveDisabled = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.NON_INTRUSIVE_INCALL, 1) == 0;
+
         final PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
         // If the screen is on, we'll prefer to not interrupt the user too much and slide in a card
-        if (pm.isScreenOn()) {
+        if (pm.isScreenOn() && !nonIntrusiveDisabled) {
             Intent intent = new Intent(mContext, InCallCardActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             mContext.startActivity(intent);
