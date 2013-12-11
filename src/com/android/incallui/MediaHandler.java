ca3dd5881023463902f9b1e339dfe314a24d3f25
Garik Badalyan <garikb@codeaurora.org>
2013-12-10 18:18:22 -0800

    MS-VT: Fix camera reconfiguration
    -Reconfigure camera after PARAM_READY only if negotiated values
     are different from existing camera settings
    
    Change-Id: Ieea1cc8113eb52f8c3a5624b6d35081c12c18bbe
    CRs-Fixed: 542241

--- src/com/android/incallui/MediaHandler.java
+++ src/com/android/incallui/MediaHandler.java
@@ -197,7 +197,7 @@ public class MediaHandler extends Handler {
     /**
      * Get Negotiated Height
      */
-    public static int getNegotiatedHeight() {
+    public synchronized static int getNegotiatedHeight() {
         Log.d(TAG, "Negotiated Height = " + mNegotiatedHeight);
         return mNegotiatedHeight;
     }
@@ -205,7 +205,7 @@ public class MediaHandler extends Handler {
     /**
      * Get Negotiated Width
      */
-    public static int getNegotiatedWidth() {
+    public synchronized static int getNegotiatedWidth() {
         Log.d(TAG, "Negotiated Width = " + mNegotiatedWidth);
         return mNegotiatedWidth;
     }
@@ -218,7 +218,7 @@ public class MediaHandler extends Handler {
         return mUIOrientationMode;
     }
 
-    public static short getNegotiatedFps() {
+    public synchronized static short getNegotiatedFps() {
         return mNegotiatedFps;
     }
 
@@ -243,10 +243,7 @@ public class MediaHandler extends Handler {
         switch (eventId) {
             case PARAM_READY_EVT:
                 Log.d(TAG, "Received PARAM_READY_EVT. Updating negotiated values");
-                mNegotiatedHeight = nativeGetNegotiatedHeight();
-                mNegotiatedWidth = nativeGetNegotiatedWidth();
-                mNegotiatedFps = nativeGetNegotiatedFPS();
-                if (mMediaEventListener != null) {
+                if (updatePreviewParams() && mMediaEventListener != null) {
                     mMediaEventListener.onParamReadyEvent();
                 }
                 break;
@@ -267,6 +264,21 @@ public class MediaHandler extends Handler {
                 Log.e(TAG, "Received unknown event id=" + eventId);
         }
 
+    }
+
+    private synchronized boolean updatePreviewParams() {
+        int h = nativeGetNegotiatedHeight();
+        int w = nativeGetNegotiatedWidth();
+        short fps = nativeGetNegotiatedFPS();
+        if (mNegotiatedHeight != h
+                || mNegotiatedWidth != w
+                || mNegotiatedFps != fps) {
+            mNegotiatedHeight = h;
+            mNegotiatedWidth = w;
+            mNegotiatedFps = fps;
+            return true;
+        }
+        return false;
     }
 
     private void processUIOrientationMode() {
