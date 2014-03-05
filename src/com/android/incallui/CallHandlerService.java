8a10e96e189ccf0c19544741731abfb21aa41169
westcripp <korukaltan@gmail.com>
2014-03-05 17:47:34 +0200

    Revert "IMS support for UI interface"
    This reverts commit d019819eb51adc9e12a2406ae5be2509f04cedc6.

--- src/com/android/incallui/CallHandlerService.java
+++ src/com/android/incallui/CallHandlerService.java
@@ -60,8 +60,6 @@ public class CallHandlerService extends Service {
     private AudioModeProvider mAudioModeProvider;
     private boolean mServiceStarted = false;
 
-    private final String LOG_TAG = "CallHandlerService";
-
     @Override
     public void onCreate() {
         Log.i(TAG, "onCreate");
